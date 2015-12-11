0884fe734074bbb102f9a129f915ff9b6d9df2af
Zyrst <zajreg@gmail.com>
2015-12-11 20:40:52 +0100

    controlls

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public GameObject _tonky;
+    private int _players;
+	// Use this for initialization
+	void Start () {
+        _players = 0;
+        GameObject go = Instantiate(_tonky);
+        go.GetComponent<Tonky>()._playerId = _players;
+        _players++;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
--- Assets/Scripts/Tonky.cs
+++ Assets/Scripts/Tonky.cs
@@ -25,7 +25,31 @@ public class Tonky : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        float x = 0f;
+        float y = 0f;
+        //if(Mathf.Abs(Input.GetAxis("Horizontal" + _playerId)) > 0.4f)
+        //{
+        //    Debug.Log("Hello " + _playerId);
+        //    x = ;
+        //}
+        //if (Mathf.Abs(Input.GetAxis("Vertical" + _playerId)) > 0.4f)
+        //{
+        //    Debug.Log("Goodbye " + _playerId);
+        //    y =;
+        //}
+        Vector3 vec = new Vector3(Input.GetAxis("Horizontal" + _playerId), -Input.GetAxis("Vertical" + _playerId),0);
+        transform.up = Vector3.Lerp(transform.up, vec, 0.05f);
+        Debug.DrawRay(transform.position, transform.up);
+
+       if((vec - transform.up).magnitude < 2f)
+       {
+           _rigidBody.AddForce((transform.up * 2f ) * vec.magnitude);
+       }
+        
+
+        //transform.Rotate(0, 0, Input.GetAxis("Horizontal" + _playerId));
+       
+        //_rigidBody.AddForce(transform.up * Input.GetAxis("Vertical" + _playerId));
 	}
 
     public void Damage(int damageAmount_, string damagingPlayerName_)
