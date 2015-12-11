9ee4258db4fe2073a420aa4f59851acfd086b529
Jeflux <jespermathiason@gmail.com>
2015-12-11 18:19:09 +0100

    bös

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : MonoBehaviour {
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+    public abstract void fire();
+}
