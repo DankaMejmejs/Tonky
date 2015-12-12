4f52c80e7b4c893b385d038393c1dbd454ef4a4b
unknown <daniel.dalebjork@hotmail.com>
2015-12-12 17:36:51 +0100

    Konfetti

---
+++ Assets/KonfettiScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KonfettiScript : MonoBehaviour {
+
+	float r,g,b;
+
+
+	// Use this for initialization
+	void Start () {
+		r = Random.Range (0.0f, 1.0f); 
+		g = Random.Range (0.0f, 1.0f); 
+		b = Random.Range (0.0f, 1.0f); 
+
+		this.GetComponent<ParticleSystem>().startColor = new Color(r, g, b, 1.0f);
+		this.GetComponent<ParticleSystem> ().loop = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		r = Random.Range (0.0f, 1.0f); 
+		g = Random.Range (0.0f, 1.0f); 
+		b = Random.Range (0.0f, 1.0f); 
+		
+		this.GetComponent<ParticleSystem>().startColor = new Color(r, g, b, 1.0f);	
+	}
+}
---
+++ Assets/KonfettiSpawnerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KonfettiSpawnerScript : MonoBehaviour {
+
+	float _lifeTime;
+	float _maxLifeTime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		_lifeTime = _maxLifeTime;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		_lifeTime -= Time.deltaTime;
+
+		if (_lifeTime <= 0) {
+			Destroy(this.gameObject);
+		}
+	
+	}
+}
