433a89f4d246c2c995292731740cc18d0bf0e13b
Zyrst <zajreg@gmail.com>
2015-12-12 12:11:02 +0100

    CircleJerk

--- Assets/Scripts/KnugOfTheHill.cs
+++ Assets/Scripts/KnugOfTheHill.cs
@@ -1,14 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class KnugOfTheHill : MonoBehaviour {
     public float radius;
+    
+    
+    private CircleCollider2D _collider;
+    public GameObject _owner;
+    
+   
 	// Use this for initialization
 	void Start () {
-	
+        _owner = null;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("HEllo");
+        if(_owner == null)
+        {
+            if(col.gameObject.name.Contains("Tonky"))
+            {
+                _owner = col.gameObject;
+            }
+        }
+    }
 }
